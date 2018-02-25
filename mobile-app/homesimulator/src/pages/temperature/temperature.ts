4442a273fad24e5284c310dfb12b4e8acffbbf99
Roderick O Smith <rsmith17@uab.edu>
2018-02-24 20:24:09 -0600

    added temperature page with temperature slider

--- mobile-app/homesimulator/src/pages/temperature/temperature.ts
+++ mobile-app/homesimulator/src/pages/temperature/temperature.ts
@@ -14,6 +14,16 @@ export class TemperaturePage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TemperaturePage');
+
+    var slider = (<HTMLInputElement>document.getElementById("myRange"));
+    var output = (<HTMLInputElement>document.getElementById("output"));
+    output.innerHTML = slider.value; // Display the default slider value
+
+  // Update the current slider value (each time you drag the slider handle)
+    slider.oninput = function() {
+    output.innerHTML = this.value;
+    }
+    
   }
 
 }
